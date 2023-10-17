676c133b089e254213c8873045b18dcc8e5d1318
chwo <chwo@bizzkit.com>
2023-10-17 09:11:18 +0200

    Init typescript scraper

---
+++ scraping/src/index.ts
@@ -0,0 +1,35 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+// Replace the placeholder with your Atlas connection string
+const uri = "mongodb://devroot:devroot@localhost:27017/";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri,  {
+        serverApi: {
+            version: ServerApiVersion.v1,
+            strict: true,
+            deprecationErrors: true,
+        }
+    }
+);
+async function run() {
+  try {
+    // Connect the client to the server (optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+
+    const myDB = client.db("myDB");
+    const myColl = myDB.collection("pizzaMenu");
+    const doc = { name: "Neapolitan pizza", shape: "round" };
+    const result = await myColl.insertOne(doc);
+    console.log(
+        `A document was inserted with the _id: ${result.insertedId}`,
+    );
+
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
